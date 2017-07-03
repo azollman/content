71c6e07315b54f8d8e3ac8938164f20d0af61a6f
asafshen <asaf@demisto.com>
2017-07-04 01:47:09 +0300

    fix content descriptor

--- setContentDescriptor.sh
+++ setContentDescriptor.sh
@@ -3,6 +3,13 @@
 git diff --name-status $2 > changelog.txt
 git diff $2 > changelog_full.txt
 
+SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/)
+
+deleted=$(cat changelog.txt | grep -p "^D")
+
+echo " #### deleted #####"
+echo "$deleted"
+echo "####"
 echo "###### changelog.txt #########"
 cat  ./changelog.txt
 echo "###############"
